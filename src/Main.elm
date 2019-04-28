b63923f6c02018f9bc79f7c039626253dba02348
Dan Bruder <ddbruder@gmail.com>
2019-04-28 15:17:38 -0400

    made it work

--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,22 @@
-module Main exposing (..)
+module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    {}
+    ( Int, Int )
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( ( 0, 0 ), Cmd.none )
 
 
 
@@ -22,12 +24,18 @@ init =
 
 
 type Msg
-    = NoOp
+    = LaunchButtonClicked
+    | LandButtonClicked
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        LaunchButtonClicked ->
+            ( ( 1200, 0 ), Cmd.none )
+
+        LandButtonClicked ->
+            ( ( 0, 0 ), Cmd.none )
 
 
 
@@ -37,8 +45,18 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        [ div [ style "display" "flex", style "position" "relative", style "flex-direction" "column", style "align-items" "center", style "justify-content" "flex-end", style "height" "100vh" ]
+            [ img
+                [ style "position" "absolute"
+                , style "bottom" ((Tuple.first model |> String.fromInt) ++ "px")
+                , style "left" ((Tuple.second model |> String.fromInt) ++ "px")
+                , style "transition" "bottom 1s ease-out 0.5s"
+                , src "/rocket.jpg"
+                ]
+                []
+            , button [ onClick LaunchButtonClicked, style "background-color" "green", style "color" "white", style "padding" "20px 30px", style "font-size" "20px", style "font-weight" "bold", style "border-radius" "10px" ] [ text "blast off" ]
+            , button [ onClick LandButtonClicked, style "background-color" "red", style "color" "white", style "padding" "20px 30px", style "font-size" "20px", style "font-weight" "bold", style "border-radius" "10px" ] [ text "come home" ]
+            ]
         ]
 
 
